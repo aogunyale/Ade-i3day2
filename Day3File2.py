c6f0241618c4da5c717c16b4d7898fc8c4af7fdc
Adebimbola Ogunyale <adebimbolaogunyale@Adebimbolas-MacBook-Pro.local>
2015-06-24 17:48:14 -0400

    Class Assignment 6-24-15

---
+++ Day3File2.py
@@ -0,0 +1,15 @@
+    
+age_of_dog = input("What is the age: ")
+print age_of_dog
+
+if age_of_dog == 0 :
+    print "Hold on! Say What"
+elif age_of_dog == 1 :
+    print "about 14 human years"
+elif age_of_dog == 2 :
+    print "about 22 human years"
+elif age_of_dog > 2 :
+    print "Human years:", 22+(age_of_dog-2)*5
+
+
+
